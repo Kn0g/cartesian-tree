f0ab5b1d8558e6bb56117227fd8349bf2e7ca566
Kn0g <74204357+Kn0g@users.noreply.github.com>
2026-07-10 15:12:20 +0200

    test: Improve tests

--- python/tests/test_angles.py
+++ python/tests/test_angles.py
@@ -22,9 +22,11 @@ def test_rpy_as_list() -> None:
     rpy = RPY(1.0, 2.0, 3.0)
     rpy_list = rpy.as_list()
     assert isinstance(rpy_list, list)
+    assert rpy_list == [1.0, 2.0, 3.0]
 
 
 def test_rpy_as_tuple() -> None:
     rpy = RPY(1.0, 2.0, 3.0)
     rpy_tuple = rpy.as_tuple()
     assert isinstance(rpy_tuple, tuple)
+    assert rpy_tuple == (1.0, 2.0, 3.0)
